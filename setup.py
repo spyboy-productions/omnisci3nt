26740e3dce0b04fa62d8c2d8585a5f2b862f736c
x9xhack <x9x@keemail.me>
2025-04-12 11:55:30 +0300

    version: update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnisci3nt",
-    version="0.1",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
