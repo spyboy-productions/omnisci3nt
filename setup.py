3744c2635b03a509c396f85ce2269e2b0fc7e194
x9xhack <x9x@keemail.me>
2025-04-12 11:57:03 +0300

    version: update setup.py version to 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnisci3nt",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
